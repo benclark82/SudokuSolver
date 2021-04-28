91005bc2a584f6e5efc4da3848d8daf93ce813dd
Ben Clark <beneclark@gmail.com>
2021-04-27 21:10:00 -0500

    Fix JUnit.  Closes #8

--- src/test/java/sudoku/NakedStrategyTest.java
+++ src/test/java/sudoku/NakedStrategyTest.java
@@ -1,9 +1,8 @@
 package sudoku;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
 class NakedStrategyTest extends Strategy {
 
